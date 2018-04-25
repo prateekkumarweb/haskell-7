0fc26ada87bc9182bd7cfda465c64acd9a039134
Om Sitapara <omsitapara23@gmail.com>
2018-04-25 16:19:47 +0530

    Add files via upload

--- src/Game.hs
+++ src/Game.hs
@@ -8,14 +8,18 @@ data State = Running | GameOver (Maybe Player) deriving (Eq, Show)
 
 type Board = Array (Int, Int) Cell
 
+
 data Game  = Game { gameBoard :: Board,
                     gamePlayer :: Player,
-                    gameState :: State
+                    gameState :: State,
+                    gameList :: [[Int]]
                   } deriving(Eq, Show)
 
 
 n :: Int
 n = 7
+
+listForCheck = [[0,0,3,1], [1,1,2,1], [2,2,1,1], [3,0,1,1], [3,4,1,1], [4,2,1,1], [5,1,2,1], [6,0,3,1]]
 
 screenWidth :: Int
 screenWidth = 640
@@ -29,17 +33,17 @@ cellWidth = fromIntegral screenWidth / fromIntegral n
 cellHeight :: Float
 cellHeight = fromIntegral screenHeight / fromIntegral n
 
-initialGame = Game { gameBoard = (array indexRange $ zip (range indexRange) (cycle [Empty])) // [ ((0, 0), Full Dot),
-                                                                                                  ((0, 3), Full Dot),
-                                                                                                  ((0, 6), Full Dot),
-                                                                                                  ((1, 1), Full Dot),
+initialGame = Game { gameBoard = (array indexRange $ zip (range indexRange) (cycle [Empty])) // [ ((0, 0), Full Player1),
+                                                                                                  ((0, 3), Full Player1),
+                                                                                                  ((0, 6), Full Player1),
+                                                                                                  ((1, 1), Full Player2),
                                                                                                   ((1, 3), Full Dot),
                                                                                                   ((1, 5), Full Dot),
                                                                                                   ((2, 2), Full Dot),
                                                                                                   ((2, 3), Full Dot),
                                                                                                   ((2, 4), Full Dot),
                                                                                                   ((3, 0), Full Dot),
-                                                                                                  ((3, 1), Full Dot),
+                                                                                                  ((3, 1), Full Player2),
                                                                                                   ((3, 2), Full Dot),
                                                                                                   ((3, 4), Full Dot),
                                                                                                   ((3, 5), Full Dot),
@@ -47,13 +51,14 @@ initialGame = Game { gameBoard = (array indexRange $ zip (range indexRange) (cyc
                                                                                                   ((4, 2), Full Dot),
                                                                                                   ((4, 3), Full Dot),
                                                                                                   ((4, 4), Full Dot),
-                                                                                                  ((5, 1), Full Dot),
+                                                                                                  ((5, 1), Full Player2),
                                                                                                   ((5, 3), Full Dot),
                                                                                                   ((5, 5), Full Dot),
                                                                                                   ((6, 0), Full Dot),
                                                                                                   ((6, 3), Full Dot),
                                                                                                   ((6, 6), Full Dot) ]
                      , gamePlayer = Player2
-                     , gameState = Running
+                     , gameState = Running,
+                     gameList = listForCheck
                    }
           where indexRange = ((0, 0), (n - 1, n - 1))
--- src/Logic.hs
+++ src/Logic.hs
@@ -1,33 +1,41 @@
 module Logic where
 
 import Data.Array
+import Game
+import Graphics.Gloss.Interface.Pure.Game
+
+
+horizontalLine game  [row, column, distance, validity]    | board!(row, column) == board!(row, column + distance) && board!(row, column) == board!(row, column + 2*distance) && board!(row, column) == player && validity == 1 = player
+                                                          | otherwise = Full Dot
+                                                                where  board = gameBoard game
+                                                                       player = Full $ gamePlayer game
+
+verticleLine game  [column, row, distance, validity]      | board!(row, column) == board!(row + distance, column) && board!(row, column) == board!(row + 2*distance, column) && board!(row, column) == player && validity == 1 = player
+                                                          | otherwise = Full Dot
+                                                                where  board = gameBoard game
+                                                                       player = Full $ gamePlayer game
+                                        
+
+
+finalHorizontalCheck game  = map (horizontalLine game) $ gameList game
+finalVerticalCheck game = map (verticleLine game) $ gameList game
+
+viewUpdate game     | (finalHorizontalCheck game)!!1 == Full Player1 = game {gameBoard = board // [((3,2), Full Player2)]}
+                    | (finalVerticalCheck game)!!1 == Full Player2 = game {gameBoard = board // [((6,6), Full Player1)]}
+                    | otherwise =  game {gameBoard = board // [((6,0), Full Player1)]}
+                        where board = gameBoard game
+
+-- listUpdater game | elem ([] : player) (finalHorizontalCheck) =  game {gameBoard = board // [((3,2), Full Player2)]}
+--                  | elem ([] : player) (finalVerticalCheck) =  game {gameBoard = board // [((6,6), Full Player2)]}
+--                  | otherwise  = game {gameBoard = board // [((6,0), Full Player1)]}
+--                     where board = gameBoard game
+--                           player = Full $ gamePlayer game
 
 
 
-type Board = Array (Int, Int) 
-n :: Int
-n = 7
+transformGame (EventKey (MouseButton LeftButton) Up _ mousePos) game = 
+    case gameState game of 
+        Running -> viewUpdate game
 
+transformGame _ game = game
 
-listOfN = take a (repeat 1)
-    where a = 18
-
-
-b :: Int
-b = 0
-
-
-createBoard =  (array indexRange $ zip (range indexRange) (repeat b))
-    where indexRange = ((0,0), (n - 1, n - 1))
-
-
-
-
-horizontalLine board [row, column, distance]    | board!(row, column) == board!(row, column + distance) && board!(row, column) == board!(row, column + 2*distance) && board!(row, column) == 1 && listOfN!!row == 1 = 1
-                                                | board!(row, column) == board!(row, column + distance) && board!(row, column) == board!(row, column + 2*distance) && board!(row, column) == 2 && listOfN!!row == 1 = 2
-                                                | otherwise = b
-    
-                                           
-
-                                         
-listForCheck = [[0,0,3], [1,1,2], [2,2,1], [3,0,1], [3,4,1], [4,2,1], [5,1,2], [6,0,3]]
